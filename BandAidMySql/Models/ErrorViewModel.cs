4b5dbfb85a6a970761420e0572730212d3e5b782
Robert Horvat <robbie_horvat@hotmail.com>
2019-05-31 19:22:58 +0200

    Add project files.

---
+++ BandAidMySql/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BandAidMySql.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
